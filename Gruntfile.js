356c52975114220c2a349c20d7942a64efe9021a
smm5g11 <st.michaylov@gmail.com>
2013-07-30 12:59:14 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-qunit');
     //grunt.registerTask('qunit', function(){console.log ('HEllo WorLD!!');})
     
-    grunt.registerTask('build');
+    grunt.registerTask('build',function(){console.log ('HEllo WorLD!!');});
     
     // Testing tasks
     grunt.registerTask('test', ['build', 'qunit']);
