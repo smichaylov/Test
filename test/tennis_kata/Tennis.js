adcbab92b1baf99ff826c9046dfbac7481f61ab0
smm5g11 <st.michaylov@gmail.com>
2013-08-06 15:03:09 +0100

    Added Tennis Kata tests

---
+++ test/tennis_kata/Tennis.js
@@ -0,0 +1,35 @@
+﻿function Player() {
+
+}
+
+Player.prototype.score = 0;
+
+Player.prototype.hasWon = false;
+
+Player.prototype.winPoint = function() {
+    if (this.score == 40) this.hasWon = true;
+    if (this.score == 30) this.score = 40;
+    else {
+        this.score += 15;
+    }
+};
+
+function Tennis() {
+
+}
+
+Tennis.prototype.players = { player1: new Player(), player2: new Player() };
+
+Tennis.prototype.playGame = function(pointWinners) {
+    for (var index in pointWinners) {
+        if (pointWinners[index] == 1) {
+            this.players.player1.winPoint();
+        } else if (pointWinners[index] == 2) {
+            this.players.player2.winPoint();
+        }
+    }
+};
+
+Tennis.prototype.isAtDeuce = function() {
+    return this.players.player1.score == 40 && this.players.player2.score == 40;
+};
---
+++ test/tennis_kata/TennisSpec.js
@@ -0,0 +1,63 @@
+﻿describe("Tennis", function () {
+    var tennis;
+    
+    beforeEach(function () {
+        tennis = new Tennis();
+    });
+
+    it("should have two players", function() {
+        expect(tennis.players.hasOwnProperty('player1')).toBeTruthy();
+        expect(tennis.players.hasOwnProperty('player2')).toBeTruthy();
+    });
+    
+    it("should start with both players scoring 0", function () {
+        expect(tennis.players.player1.score).toEqual(0);
+        expect(tennis.players.player2.score).toEqual(0);
+    });
+
+    it("should be deuce if both players have 40 points", function () {
+        tennis.playGame([1,2,1,2,1,2]);
+        expect(tennis.isAtDeuce()).toBeTruthy();
+    });
+    
+    it("should not be deuce if first player has 40 and second has 30 points", function () {
+        tennis.playGame([1, 2, 1, 2, 1]);
+        expect(tennis.isAtDeuce()).toBeFalsy();
+    });
+});
+
+describe("Player", function () {
+    var player;
+
+    beforeEach(function() {
+        player = new Player();
+    });
+    
+    it("should have score of 15 after winning a point", function () {
+        player.winPoint();
+        expect(player.score).toEqual(15);
+    });
+    
+    it("should have score of 30 after winning 2 points", function () {
+        player.winPoint();
+        player.winPoint();
+        expect(player.score).toEqual(30);
+    });
+    
+    it("should have score of 40 after winning 3 points", function () {
+        player.winPoint();
+        player.winPoint();
+        player.winPoint();
+        expect(player.score).toEqual(40);
+    });
+    
+    it("should be the winner after winning 4 points", function () {
+        player.winPoint();
+        player.winPoint();
+        player.winPoint();
+        player.winPoint();
+        expect(player.hasWon).toEqual(true);
+    });
+
+
+});
